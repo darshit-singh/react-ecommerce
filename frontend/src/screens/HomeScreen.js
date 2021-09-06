45a13ce25f2360fd007bb58ebd4c213cef449442
Darshit Sarna <darshit.sarna@pristyncare.com>
2021-09-06 20:40:18 +0530

    some formatting changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,9 +9,9 @@ const HomeScreen = () => {
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => {
-                return( 
+                    return (
                         <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                            <Product product={product}/>
+                            <Product product={product} />
                         </Col>
                     )
                 })}
